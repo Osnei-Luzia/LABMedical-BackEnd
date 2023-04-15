1dc075cf2f207598e17f36036d2c3a69dab6a675
Osnei Luzia <osnei.luzia@gmail.com>
2023-04-15 14:56:37 -0300

    Setup Inicial

---
+++ projeto/LabMedical-BackEnd/src/main/java/projeto/labmedicalbackend/LabMedicalBackEndApplication.java
@@ -0,0 +1,13 @@
+package projeto.labmedicalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabMedicalBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabMedicalBackEndApplication.class, args);
+    }
+
+}
